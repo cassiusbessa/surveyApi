71c9b181c365a2e2f1d70a2be3e2afec975bd8dd
cassiusbessa <cassiusbessa@gmail.com>
2022-12-24 05:38:06 -0300

    test: :white_check_mark: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controller/signup.spec.ts
@@ -0,0 +1,14 @@
+import { SingUpController } from './signup'
+describe('SignUp Controller', () => {
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SingUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
