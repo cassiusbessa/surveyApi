e95b6b9649546ddaacb69d334aac456e2b0a6444
cassiusbessa <cassiusbessa@gmail.com>
2022-12-24 07:09:37 -0300

    refactor: :art: validate dynamically required fields in SingupController.handle

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -3,11 +3,11 @@ import { HttpRequest, HttpResponse } from '../protocols/http'
 import { badRequest } from '../helpers/http-helper'
 export class SingUpController {
   handle (request: HttpRequest): HttpResponse {
-    if (!request.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!request.body.email) {
-      return badRequest(new MissingParamError('email'))
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      if (!request.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
     return {
       statusCode: 200,
