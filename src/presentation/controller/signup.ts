be15d6946b76258782543681715ce0ef1bddaa05
cassiusbessa <cassiusbessa@gmail.com>
2022-12-24 09:02:30 -0300

    feat: :sparkles: ensure SignUpController.handler returns 400 if an invalid email is provided

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -2,13 +2,25 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email-validator'
+import { InvalidParamError } from '../errors/invalid-param-error'
 export class SingUpController implements Controller {
-  handle (request: HttpRequest): HttpResponse {
+  private readonly emailValidator: EmailValidator
+  constructor (emailValidor: EmailValidator) {
+    this.emailValidator = emailValidor
+  }
+
+  handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
-      if (!request.body[field]) {
+      if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
+    }
+
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
     }
     return {
       statusCode: 200,
