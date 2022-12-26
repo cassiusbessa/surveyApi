f2ed9f7d5408c8454dac66767331e4a319b3874f
cassiusbessa <cassiusbessa@gmail.com>
2022-12-26 06:16:29 -0300

    chore: change test scripts

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  coverageDirectory: 'coverage',
-  collectCoverage: true,
-  // coverageProvider: 'v8',
+  // coverageDirectory: 'coverage',
+  // collectCoverage: true,
+  // // coverageProvider: 'v8',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
