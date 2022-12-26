a4ff675498ca00e3059cce683366c98e363116e5
cassiusbessa <cassiusbessa@gmail.com>
2022-12-26 19:17:15 -0300

    feat: :sparkles: ensure EmailValidatorAdapter returns false if validator returns false

---
+++ src/utils/email-validator.ts
@@ -0,0 +1,6 @@
+import { EmailValidator } from '../presentation/protocols'
+export class EmailValidatorAdapter implements EmailValidator {
+  isValid (email: string): boolean {
+    return false
+  }
+}
