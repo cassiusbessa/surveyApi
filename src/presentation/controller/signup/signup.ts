13ff9119c629f9d93c79128c5e6de3cf5b7de22f
cassiusbessa <cassiusbessa@gmail.com>
2022-12-26 05:12:19 -0300

    feat: :sparkles: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controller/signup/signup.ts
+++ src/presentation/controller/signup/signup.ts
@@ -26,14 +26,14 @@ export class SingUpController implements Controller {
       if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
-      this.addAccount.add({
+      const account = this.addAccount.add({
         name,
         email,
         password
       })
       return {
         statusCode: 200,
-        body: {}
+        body: account
       }
     } catch (error) {
       return serverError()
