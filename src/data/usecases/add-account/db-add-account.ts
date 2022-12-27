89d98ee0ba37209c0b4665701577369fc3e7c007
cassiusbessa <cassiusbessa@gmail.com>
2022-12-27 01:21:01 -0300

    feat: :sparkles: ensure DbAddAccount calls AddAccountRepository with correct values

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,19 +1,21 @@
-import { AddAccount, AddAccountModel, Encrypter, AccountModel } from './db-add-account-protocols'
+import { AddAccount, AddAccountModel, Encrypter, AccountModel, AddAccountRepository } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
-  constructor (encrypter: Encrypter) {
+  private readonly addAccountRepository: AddAccountRepository
+  constructor (encrypter: Encrypter, addAccountRepository: AddAccountRepository) {
     this.encrypter = encrypter
+    this.addAccountRepository = addAccountRepository
   }
 
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const newAccount = {
-      id: 'valid_id',
       name: accountData.name,
       email: accountData.email,
       password: accountData.password
     }
-    await this.encrypter.encrypt(accountData.password)
-    return await new Promise(resolve => resolve(newAccount))
+    const hashedPassword = await this.encrypter.encrypt(accountData.password)
+    const account = await this.addAccountRepository.add({ ...newAccount, password: hashedPassword })
+    return await new Promise(resolve => resolve({ ...newAccount, id: account.id }))
   }
 }
