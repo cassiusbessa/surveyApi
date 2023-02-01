3e7b65d36f562b0921a9660d353eb8e2ecda5f61
cassiusbessa <cassiusbessa@gmail.com>
2023-02-01 04:00:46 -0300

    feat: :sparkles: ensure LogControllerDecorator calls controller with correct value

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,12 +3,15 @@ import { EmailValidatorAdapter } from './../../utils/email-validator-adapter'
 import { DbAddAccount } from './../../data/usecases/add-account/db-add-account'
 import { AccountMongoRepository } from './../../infra/db/mongodb/account-repository/account'
 import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
+import { LogControllerDecorator } from '../decorators/log'
+import { Controller } from '../../presentation/protocols'
 
-export const makeSingUpController = (): SingUpController => {
+export const makeSingUpController = (): Controller => {
   const salt = 12
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-  return new SingUpController(emailValidatorAdapter, dbAddAccount)
+  const signUpController = new SingUpController(emailValidatorAdapter, dbAddAccount)
+  return new LogControllerDecorator(signUpController)
 }
