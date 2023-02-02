4b2c940dd4c7032642682f5ae213960b4e2d05cf
cassiusbessa <cassiusbessa@gmail.com>
2023-02-02 03:13:29 -0300

    feat: :sparkles: add logMongoRepository to signup factory

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -5,6 +5,7 @@ import { AccountMongoRepository } from './../../infra/db/mongodb/account-reposit
 import { BcryptAdapter } from '../../infra/cryptography/bcrypt-adapter'
 import { LogControllerDecorator } from '../decorators/log'
 import { Controller } from '../../presentation/protocols'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 
 export const makeSingUpController = (): Controller => {
   const salt = 12
@@ -13,5 +14,6 @@ export const makeSingUpController = (): Controller => {
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
   const signUpController = new SingUpController(emailValidatorAdapter, dbAddAccount)
-  return new LogControllerDecorator(signUpController)
+  const logMongoRepository = new LogMongoRepository()
+  return new LogControllerDecorator(signUpController, logMongoRepository)
 }
