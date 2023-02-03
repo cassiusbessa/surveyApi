ccd48f20035328f214fdc5305b879834d4cadb26
cassiusbessa <cassiusbessa@gmail.com>
2023-02-03 05:47:24 -0300

    test: :white_check_mark: ensure LoginController returns 401 if invalid credential are provided

--- src/presentation/controller/login/login.spec.ts
+++ src/presentation/controller/login/login.spec.ts
@@ -1,6 +1,6 @@
 import { Authentication } from '../../../domain/usecases/authentication'
 import { InvalidParamError, MissingParamError, ServerError } from '../../errors'
-import { badRequest, serverError } from '../../helpers/http-helper'
+import { badRequest, serverError, unauthorized } from '../../helpers/http-helper'
 import { EmailValidator, HttpRequest } from '../../protocols'
 import { LoginController } from './login'
 
@@ -111,5 +111,13 @@ describe('Login Controller', () => {
     const authenticationSpy = jest.spyOn(authenticationStub, 'auth')
     await sut.handle(httpRequest)
     expect(authenticationSpy).toHaveBeenCalledWith(httpRequest.body.email, httpRequest.body.password)
+  })
+
+  test('Should return 401 if invalid credentials are provided', async () => {
+    const { sut, authenticationStub } = makeSut()
+    jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(new Promise(resolve => resolve('')))
+    const httpRequest = makeFakeRequest()
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(unauthorized())
   })
 })
