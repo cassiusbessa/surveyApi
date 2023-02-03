fe881c2fc7b9a7cd650c26c106d69a9083623923
cassiusbessa <cassiusbessa@gmail.com>
2023-02-03 05:57:54 -0300

    feat: :sparkles: ensure LoginController returns 200 if valid credential are provided

--- src/presentation/controller/login/login.ts
+++ src/presentation/controller/login/login.ts
@@ -1,6 +1,6 @@
 import { Authentication, Controller, EmailValidator, HttpRequest, HttpResponse } from './login-protocols'
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest, serverError, unauthorized } from '../../helpers/http-helper'
+import { badRequest, ok, serverError, unauthorized } from '../../helpers/http-helper'
 
 export class LoginController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -27,7 +27,7 @@ export class LoginController implements Controller {
       if (!accessToken) {
         return unauthorized()
       }
-      return { statusCode: 200, body: {} }
+      return ok({ accessToken })
     } catch (error) {
       return serverError(error)
     }
