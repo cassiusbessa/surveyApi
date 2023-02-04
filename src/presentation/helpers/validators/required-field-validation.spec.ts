ac77669a45b2536c0a83dc33da489a98918a0ad0
cassiusbessa <cassiusbessa@gmail.com>
2023-02-04 12:51:51 -0300

    refactor: :art: specifie the type of error returned

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -1,3 +1,4 @@
+import { MissingParamError } from '../../errors'
 import { RequiredFieldValidation } from './required-field-validation'
 
 const makeSut = (): RequiredFieldValidation => {
@@ -8,7 +9,7 @@ describe('RequiredFieldValidation', () => {
   test('Should return an error if field is not provided', () => {
     const sut = makeSut()
     const error = sut.validate({ name: 'any_name' })
-    expect(error).toEqual(new Error('Missing param: any_field'))
+    expect(error).toEqual(new MissingParamError('any_field'))
   })
 
   test('Should not return if field is provided', () => {
