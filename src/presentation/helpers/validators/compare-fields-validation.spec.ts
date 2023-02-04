390525b4864cd74e94f7bf7e5cb61a3adadf054e
cassiusbessa <cassiusbessa@gmail.com>
2023-02-04 13:01:34 -0300

    test: ensure CompareFieldsValidation returns a InvalidParamError if validation fail

---
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -0,0 +1,14 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './compare-fields-validation'
+
+const makeSut = (): CompareFieldsValidation => {
+  return new CompareFieldsValidation('any_field', 'other_field')
+}
+
+describe('CompareFieldsValidation', () => {
+  test('Should return an error if field is not provided', () => {
+    const sut = makeSut()
+    const error = sut.validate({ any_field: 'any_value', other_field: 'other_value' })
+    expect(error).toEqual(new InvalidParamError('other_field'))
+  })
+})
