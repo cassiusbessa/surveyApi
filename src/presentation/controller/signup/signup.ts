5ebf028aaa9ce1969ec28e5941503e364100402d
cassiusbessa <cassiusbessa@gmail.com>
2023-02-04 11:23:36 -0300

    feat: :sparkles: ensure SignupController calls validation with correct value

--- src/presentation/controller/signup/signup.ts
+++ src/presentation/controller/signup/signup.ts
@@ -1,17 +1,20 @@
 
 import { MissingParamError, InvalidParamError } from '../../errors'
 import { badRequest, ok, serverError } from '../../helpers/http-helper'
-import { HttpRequest, HttpResponse, Controller, EmailValidator, AddAccount } from './signup-protocols'
+import { HttpRequest, HttpResponse, Controller, EmailValidator, AddAccount, Validation } from './signup-protocols'
 export class SingUpController implements Controller {
   private readonly emailValidator: EmailValidator
   private readonly addAccount: AddAccount
-  constructor (emailValidor: EmailValidator, addAccount: AddAccount) {
+  private readonly validation: Validation
+  constructor (emailValidor: EmailValidator, addAccount: AddAccount, validation: Validation) {
     this.emailValidator = emailValidor
     this.addAccount = addAccount
+    this.validation = validation
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate(httpRequest.body)
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
