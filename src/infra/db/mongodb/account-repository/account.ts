67a835120fdf5c99fa7db1c7385cbd905949400f
cassiusbessa <cassiusbessa@gmail.com>
2023-02-05 04:52:18 -0300

    fix: :bug: generate id in infra layer instead of automatically in mongodb

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -2,11 +2,12 @@ import { AddAccountRepository } from '../../../../data/protocols/add-account-rep
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
+import { ObjectId } from 'mongodb'
 
 export class AccountMongoRepository implements AddAccountRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollections('accounts')
-    const result = await accountCollection.insertOne(accountData)
+    const result = await accountCollection.insertOne({ _id: new ObjectId(), ...accountData })
     const account = { id: result.insertedId.toString(), ...accountData }
     return account
   }
