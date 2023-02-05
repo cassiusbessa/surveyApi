0d5de855caf8080a5bf46340ac4d2278dff11bf1
cassiusbessa <cassiusbessa@gmail.com>
2023-02-05 05:13:11 -0300

    refactor: :art: change tests mensages

--- src/presentation/helpers/validators/compare-fields-validation.spec.ts
+++ src/presentation/helpers/validators/compare-fields-validation.spec.ts
@@ -6,12 +6,12 @@ const makeSut = (): CompareFieldsValidation => {
 }
 
 describe('CompareFieldsValidation', () => {
-  test('Should return an error if field is not provided', () => {
+  test('Should return an InvalidParamError if field is not provided', () => {
     const sut = makeSut()
     const error = sut.validate({ any_field: 'any_value', other_field: 'other_value' })
     expect(error).toEqual(new InvalidParamError('other_field'))
   })
-  test('Should not return if validation succeeds', () => {
+  test('Should return null if validation succeeds', () => {
     const sut = makeSut()
     const error = sut.validate({ any_field: 'any_value', other_field: 'any_value' })
     expect(error).toBeFalsy()
--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -6,13 +6,13 @@ const makeSut = (): RequiredFieldValidation => {
 }
 
 describe('RequiredFieldValidation', () => {
-  test('Should return an error if field is not provided', () => {
+  test('Should return a MissingParamError if field is not provided', () => {
     const sut = makeSut()
     const error = sut.validate({ name: 'any_name' })
     expect(error).toEqual(new MissingParamError('any_field'))
   })
 
-  test('Should not return if field is provided', () => {
+  test('Should return null if field is provided', () => {
     const sut = makeSut()
     const error = sut.validate({ any_field: 'any_value' })
     expect(error).toBeFalsy()
