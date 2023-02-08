bfbde7d5f1b6ba140b2844ff805094ed61a6e727
cassiusbessa <cassiusbessa@gmail.com>
2023-02-08 04:27:03 -0300

    test: ensure DbAuthentication returns falsy if LoadAccountByEmailRepository returns falsy

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -52,4 +52,11 @@ describe('DbAuthentication UseCase', () => {
     const promise = sut.auth(makeFakeAuthentication())
     await expect(promise).rejects.toThrow()
   })
+
+  test('Should return falsy if LoadAccountByEmailRepository returns falsy', async () => {
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
+    jest.spyOn(loadAccountByEmailRepositoryStub, 'loadByEmail').mockReturnValueOnce(new Promise(resolve => resolve(null)))
+    const accessToken = await sut.auth(makeFakeAuthentication())
+    expect(accessToken).toBeFalsy()
+  })
 })
