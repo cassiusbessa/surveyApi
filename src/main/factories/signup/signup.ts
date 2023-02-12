56fc375d7554b5842c4e44f0740344f255095d45
cassiusbessa <cassiusbessa@gmail.com>
2023-02-12 01:57:42 -0300

    refactor: :art: move bcrypt-adapter files to a folder

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,7 +1,7 @@
 import { SingUpController } from '../../../presentation/controller/signup/signup'
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
-import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter'
+import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter/bcrypt-adapter'
 import { LogControllerDecorator } from '../../decorators/log'
 import { Controller } from '../../../presentation/protocols'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
