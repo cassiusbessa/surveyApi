a39acbdafb44ef37d7512f9f28716467ab6bb849
cassiusbessa <cassiusbessa@gmail.com>
2023-02-12 02:18:46 -0300

    test: :white_check_mark: ensure JwtAdapter calls sign with correct values

---
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.spec.ts
@@ -0,0 +1,10 @@
+import { JwtAdapter } from './jwt-adapter'
+import jwt from 'jsonwebtoken'
+describe('JwtAdapter', () => {
+  test('Should call sign with correct values', async () => {
+    const sut = new JwtAdapter('secret')
+    const signSpy = jest.spyOn(jwt, 'sign')
+    await sut.encrypt('any_id')
+    expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secret')
+  })
+})
