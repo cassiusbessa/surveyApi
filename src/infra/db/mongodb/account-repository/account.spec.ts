2d7fa36d18fdde49b66ebd45e14bc120dad7bf0e
cassiusbessa <cassiusbessa@gmail.com>
2023-02-17 21:44:39 -0300

    test: :white_check_mark: ensure AccountMongoRepository returns null if loadByEmail returns null

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -49,4 +49,10 @@ describe('Account Mongo Repository', () => {
     expect(account?.email).toBe('valid_email@email.com')
     expect(account?.password).toBe('valid_password')
   })
+
+  test('Should return null if loadByEmail fails', async () => {
+    const sut = makeSut()
+    const account = await sut.loadByEmail('invalid_email')
+    expect(account).toBeFalsy()
+  })
 })
