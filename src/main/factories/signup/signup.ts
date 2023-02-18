5e877fab05ad9fd32d0fe2113d59d281c17ba2db
cassiusbessa <cassiusbessa@gmail.com>
2023-02-17 23:13:31 -0300

    refactor: :art: improve file names

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,10 +1,10 @@
 import { SingUpController } from '../../../presentation/controller/signup/signup'
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
-import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
 import { BcryptAdapter } from '../../../infra/cryptography/bcrypt-adapter/bcrypt-adapter'
-import { LogControllerDecorator } from '../../decorators/log'
+import { LogControllerDecorator } from '../../decorators/log-controller-decorator'
 import { Controller } from '../../../presentation/protocols'
-import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
+import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
 import { makeSignUpValidation } from '../signup/signup-validation'
 
 export const makeSingUpController = (): Controller => {
--- src/presentation/controller/login/login-controller.ts
+++ src/presentation/controller/login/login-controller.ts
@@ -1,5 +1,5 @@
-import { Validation } from './../../protocols/validation'
-import { Authentication, Controller, HttpRequest, HttpResponse } from './login-protocols'
+import { Validation } from '../../protocols/validation'
+import { Authentication, Controller, HttpRequest, HttpResponse } from './login-controller-protocols'
 import { badRequest, ok, serverError, unauthorized } from '../../helpers/http/http-helper'
 
 export class LoginController implements Controller {
