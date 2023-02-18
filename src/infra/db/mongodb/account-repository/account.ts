b9d3b9794a38bc4ab8f430f99ef67b6884421d79
cassiusbessa <cassiusbessa@gmail.com>
2023-02-17 21:39:53 -0300

    feat: :sparkles: ensure AccountMongoRepository returns an account on loadbyemail success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -3,12 +3,20 @@ import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
 import { ObjectId } from 'mongodb'
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollections('accounts')
     const result = await accountCollection.insertOne({ _id: new ObjectId(), ...accountData })
     const account = { id: result.insertedId.toString(), ...accountData }
+    return account
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel | null> {
+    const accountCollection = MongoHelper.getCollections('accounts')
+    const result = await accountCollection.findOne({ email }) as any
+    const account = { id: result?._id, ...result }
     return account
   }
 }
